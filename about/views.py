282e02a6fb1f2d7ffff1a08da454318a71fbec08
Jshnee <203004294+JamieJB123@users.noreply.github.com>
2025-08-06 12:27:42 +0100

    Create about view: add logic to deal with form posts and return necessary data to the view. Add initial template to about view

--- about/views.py
+++ about/views.py
@@ -1,5 +1,23 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import About, CollaborateRequest
+from django.contrib import messages
 
 # Create your views here.
 def about(request):
-    return HttpResponse("About page successfully wired up!")
+
+    if request.method == "POST":
+        form = CollaborateRequest(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, "Collaboration request sent successfully!")
+            return redirect('about')
+        else:
+            messages.add_message(request, messages.ERROR, "Error sending collaboration request. Please try again.")
+
+    about = About.objects.all().order_by("-updated_on").first()
+    form = CollaborateRequest()
+    
+    return render(request, 'about/about.html', {
+        'about': about,
+        'form': form,
+    })
