d911333a1b2b3cfb3d31a0bdf5693d28252962e2
Jshnee <203004294+JamieJB123@users.noreply.github.com>
2025-08-06 15:17:22 +0100

    Create about template. Tweak about view to fix form display issues. Include fallback image for about page in static files.

--- about/views.py
+++ about/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render, redirect
-from .models import About, CollaborateRequest
 from django.contrib import messages
+from .models import About
+from .forms import FeedbackForm
+
 
 # Create your views here.
 def about(request):
 
     if request.method == "POST":
-        form = CollaborateRequest(request.POST)
+        form = FeedbackForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.add_message(request, messages.SUCCESS, "Collaboration request sent successfully!")
+            messages.add_message(request, messages.SUCCESS, "Contact form sent successfully!")
             return redirect('about')
         else:
-            messages.add_message(request, messages.ERROR, "Error sending collaboration request. Please try again.")
+            messages.add_message(request, messages.ERROR, "Error sending contact form. Please try again.")
 
     about = About.objects.all().order_by("-updated_on").first()
-    form = CollaborateRequest()
-    
+    form = FeedbackForm()
+
     return render(request, 'about/about.html', {
         'about': about,
         'form': form,
